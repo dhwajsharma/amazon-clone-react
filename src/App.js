4a052cfe2d277f20463080df9042b445af303564
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-16 22:20:40 +0530

    done setting up react router.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h2>checkout</h2>
+          </Route>
+          <Route path="/login">
+            <h2>login</h2>
+          </Route>
+          <Route path="/" exact>
+            <h2>Home page</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 
-    </div>
   );
 }
 
